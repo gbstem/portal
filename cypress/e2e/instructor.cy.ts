cb1e69a5618e44f7527daf75e56796b3e89abfbf
Darren Meyer <dmeyer246@gmail.com>
2026-06-12 21:45:46 -0400

    Improved instructor Cypress test coverage, but with 2 disabled due to session polluting I haven't debugged yet

--- cypress/e2e/instructor.cy.ts
+++ cypress/e2e/instructor.cy.ts
@@ -1,7 +1,7 @@
 import { generateDateHash } from '../support/utils'
 
 describe('Section C & E: Instructor Applications & Community Service', () => {
-  it.only('Test Case 8: Instructor Application Submission', () => {
+  it('Test Case 8: Instructor Application Submission', () => {
     const email = `${generateDateHash('inst')}@gbstem.org`
     cy.loadSignupPage()
     cy.selectOption(
@@ -16,7 +16,9 @@ describe('Section C & E: Instructor Applications & Community Service', () => {
     cy.get('button[type="submit"]').click()
 
     // Handle email verification dialog
-    cy.get('[role="dialog"]').contains('button', 'Go to dashboard').click()
+    cy.get('[role="dialog"]', { timeout: 10000 })
+      .contains('button', 'Go to dashboard')
+      .click()
     cy.getLatestOobLink(email, 'VERIFY_EMAIL').then((link) => {
       cy.request(link)
     })
@@ -99,8 +101,103 @@ describe('Section C & E: Instructor Applications & Community Service', () => {
       .should('have.value', 'Some programming experience')
   })
 
+  it('Test Case 8b: Instructor Application Status - Accepted', () => {
+    cy.signedInSession('instructor')
+
+    cy.get('body').should('contain', 'Your Classes')
+    cy.get('a').contains('Community Service Hours Tracker').should('be.visible')
+
+    cy.visit('/apply')
+    cy.wait(1500)
+    cy.get('body').should('contain', 'Application submitted and in review!')
+    cy.get('input[name="personal.phoneNumber"]').should('be.disabled')
+  })
+
+  // TODO(dmeyer246) Figure out why enabling this causes the rest of the tests to fail,
+  // due to polluted sessions.
+  it.skip('Test Case 8c: Instructor Application Status - Rejected', () => {
+    cy.signedInSession('instructor', {
+      email: 'instructor-rejected@gbstem.org',
+    })
+
+    cy.get('body').contains(
+      /we were not able to accept you as an instructor for gbSTEM/,
+    )
+    cy.get('a').contains('Community Service Hours Tracker').should('not.exist')
+
+    cy.visit('/apply')
+    cy.wait(1500)
+    cy.get('body').should('contain', 'Application submitted and in review!')
+    cy.signOutViaUi()
+  })
+
+  // TODO(dmeyer246) Figure out why enabling this causes the rest of the tests to fail,
+  // due to polluted sessions.
+  it.skip('Test Case 8d: Instructor Interview Slot Booking & Time Request', () => {
+    cy.signedInSession('instructor', {
+      email: 'instructor-interview@gbstem.org',
+    })
+
+    cy.get('body').should('contain', 'Available Interview Slots')
+
+    // Scenario 1: Request time
+    cy.contains('button', 'Request A Time').click()
+    cy.fillInput('input[name="dateToAdd"]', '2026-06-15T15:00')
+    cy.get('input[name="dateToAdd"]')
+      .closest('form')
+      .contains('button', 'Submit')
+      .click()
+    cy.waitForNotification('Thank you for requesting a new timeslot!')
+
+    // Scenario 2: Book slot
+    cy.visit('/dashboard')
+    cy.wait(1500)
+    cy.get('input[type="radio"]')
+      .first()
+      .parent()
+      .invoke('text')
+      .then((labelText) => {
+        const expectedDate = labelText.split(' (')[0].trim()
+
+        cy.get('input[type="radio"]').first().check({ force: true })
+        cy.get('input[type="radio"]')
+          .closest('form')
+          .contains('button', 'Submit')
+          .click()
+        cy.waitForNotification('Thank you for signing up for an interview!')
+
+        cy.get('body')
+          .contains(/Your interview will be on/)
+          .invoke('text')
+          .then((text) => {
+            const match = text.match(/Your interview will be on (.*?) with/)
+            const capturedDate = match ? match[1].trim() : ''
+            expect(capturedDate).to.equal(expectedDate)
+          })
+      })
+    cy.signOutViaUi()
+  })
+
+  it('Test Case 10b: Instructor Submit Attendance Feedback', () => {
+    cy.signedInSession('instructor')
+
+    cy.contains('button', 'Submit Feedback').click()
+    cy.get('[role="dialog"]').within(() => {
+      cy.contains(/class feedback form/i).should('be.visible')
+      cy.get('input[name="classDate"]').should('not.be.disabled')
+      cy.fillInput('input[name="classDate"]', '2026-06-12')
+      cy.fillInput('input[name="classNumber"]', '1')
+      cy.fillInput(
+        'input[name="feedback"]',
+        'Class went really well! Students were highly interactive.',
+      )
+      cy.get('input[type="checkbox"]').first().check({ force: true }) // Check attendance for first student
+      cy.contains('button', 'Submit').click({ force: true })
+    })
+    cy.waitForNotification('Class Feedback saved!')
+  })
+
   it('Test Case 11: Instructor Community Service Hours', () => {
-    // Sign in as existing accepted instructor
     cy.signedInSession('instructor', { initialPage: '/community-service' })
 
     // Click confirm hours button and verify email notification toast
@@ -108,5 +205,73 @@ describe('Section C & E: Instructor Applications & Community Service', () => {
       .should('not.be.disabled')
       .click()
     cy.waitForNotification('Email sent successfully!')
+  })
+
+  it('Test Case 13: Class Details Submission & Modifying Details', () => {
+    cy.signedInSession('instructor')
+
+    // Edit Form Details
+    cy.contains('h2', 'Class Details')
+      .closest('.rounded-xl')
+      .within(() => {
+        cy.contains('button', 'Edit class details').click()
+        cy.fillInput('input[name="classCap"]', '8')
+        cy.get('button[type="submit"]').click()
+      })
+    cy.waitForNotification('Class details saved!')
+
+    // Wait for the 2-second timeout reload to trigger and load the page
+    cy.wait(2500)
+    cy.contains('h2', 'Class Details')
+      .closest('.rounded-xl')
+      .within(() => {
+        cy.get('input[name="classCap"]')
+          .should('be.disabled')
+          .should('have.value', '8')
+      })
+  })
+
+  it('Test Case 14: Edit Schedule and Add Class', () => {
+    cy.signedInSession('instructor')
+
+    // Edit Schedule & Delete Class Session
+    cy.contains('button', 'Edit Schedule').click()
+    cy.get('input[type="datetime-local"]').first().should('be.visible')
+    cy.contains('button', 'Delete').first().click()
+    cy.contains('button', 'Save Changes').click()
+
+    // Handle notify parents modal
+    cy.get('[role="dialog"]').should('contain', "notify your student's parents")
+    cy.contains('button', 'Close').click()
+    cy.get('[role="dialog"]').should('not.exist')
+
+    // Add Class Session
+    cy.contains('button', 'Add Class to Schedule').click()
+    cy.get('[role="dialog"]').within(() => {
+      cy.fillInput('input[type="datetime-local"]', '2026-06-20T11:00')
+      cy.contains('button', 'Add Class').click({ force: true })
+    })
+    cy.waitForNotification('Meeting times updated!')
+
+    // Close the warning dialog to notify parents.
+    cy.get('[role="dialog"]').should('contain', "notify your student's parents")
+    cy.contains('button', 'Close').click()
+    cy.get('[role="dialog"]').should('not.exist')
+
+    // Verify it worked
+    cy.get('body').should('contain', 'June 20')
+  })
+
+  it('Test Case 15: Request Sub', () => {
+    cy.signedInSession('instructor')
+
+    // Request Sub
+    cy.contains('button', 'Request Sub').first().click()
+    cy.get('[role="dialog"]').should('contain', 'Submit A Sub Request')
+    cy.get('[role="dialog"]')
+      .find('input[type="text"]')
+      .type('Sub to cover lists and loops')
+    cy.contains('button', 'Confirm Request').click({ force: true })
+    cy.waitForNotification('Sub request sent!')
   })
 })
